968ab7b850d4193dfd76ea97cb3d94f2e0ccb8a3
weiyx <weason1998@gmail.com>
2022-05-20 23:21:02 +0800

    add tlpool and brpool

--- mmdet/models/dense_heads/reppoints_v2_head.py
+++ mmdet/models/dense_heads/reppoints_v2_head.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from mmcv.cnn import ConvModule, bias_init_with_prob, normal_init
-from mmcv.ops import DeformConv2d, TLPool, BRPool
+from mmcv.ops import DeformConv2d, CornerPool
 
 from mmdet.core import (PointGenerator, build_assigner, build_sampler,
                         images_to_levels, multi_apply, multiclass_nms, unmap)
@@ -13,6 +13,75 @@ from .anchor_free_head import AnchorFreeHead
 from mmdet.utils.instances import Instances
 from mmdet.utils.common import compute_locations
 from itertools import accumulate
+
+class CornerPoolPack(nn.Module):
+    def __init__(self, dim, pool1, pool2, conv_cfg=None, norm_cfg=None, first_kernel_size=3, kernel_size=3, corner_dim=128):
+        super(CornerPoolPack, self).__init__()
+        self.p1_conv1 = ConvModule(
+            dim,
+            corner_dim,
+            first_kernel_size,
+            stride=1,
+            padding=(first_kernel_size - 1) // 2,
+            conv_cfg=conv_cfg,
+            norm_cfg=norm_cfg)
+        self.p2_conv1 = ConvModule(
+            dim,
+            corner_dim,
+            first_kernel_size,
+            stride=1,
+            padding=(first_kernel_size - 1) // 2,
+            conv_cfg=conv_cfg,
+            norm_cfg=norm_cfg)
+
+        self.p_conv1 = nn.Conv2d(corner_dim, dim, 3, padding=1, bias=False)
+        self.p_gn1   = nn.GroupNorm(num_groups=32, num_channels=dim)
+
+        self.conv1 = nn.Conv2d(dim, dim, 1, bias=False)
+        self.gn1   = nn.GroupNorm(num_groups=32, num_channels=dim)
+        self.relu1 = nn.ReLU(inplace=True)
+
+        self.conv2 = ConvModule(
+            dim,
+            dim,
+            kernel_size,
+            stride=1,
+            padding=(kernel_size - 1) // 2,
+            conv_cfg=conv_cfg,
+            norm_cfg=norm_cfg)
+
+        self.pool1 = pool1
+        self.pool2 = pool2
+
+    def forward(self, x):
+        # pool 1
+        p1_conv1 = self.p1_conv1(x)
+        pool1    = self.pool1(p1_conv1)
+
+        # pool 2
+        p2_conv1 = self.p2_conv1(x)
+        pool2    = self.pool2(p2_conv1)
+
+        # pool 1 + pool 2
+        p_conv1 = self.p_conv1(pool1 + pool2)
+        p_gn1   = self.p_gn1(p_conv1)
+
+        conv1 = self.conv1(x)
+        gn1   = self.gn1(conv1)
+        relu1 = self.relu1(p_gn1 + gn1)
+
+        conv2 = self.conv2(relu1)
+        return conv2
+
+
+class TLPool(CornerPoolPack):
+    def __init__(self, dim, conv_cfg=None, norm_cfg=None, first_kernel_size=3, kernel_size=3, corner_dim=128):
+        super(TLPool, self).__init__(dim, CornerPool('top'), CornerPool('left'), conv_cfg, norm_cfg, first_kernel_size, kernel_size, corner_dim)
+
+
+class BRPool(CornerPoolPack):
+    def __init__(self, dim, conv_cfg=None, norm_cfg=None, first_kernel_size=3, kernel_size=3, corner_dim=128):
+        super(BRPool, self).__init__(dim, CornerPool('bottom'), CornerPool('right'), conv_cfg, norm_cfg, first_kernel_size, kernel_size, corner_dim)
 
 
 @HEADS.register_module()
