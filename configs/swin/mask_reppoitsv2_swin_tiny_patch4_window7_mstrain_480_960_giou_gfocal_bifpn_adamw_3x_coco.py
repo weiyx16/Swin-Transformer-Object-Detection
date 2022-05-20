abd36afa0bcba9efbf072d987f1707c0a7b3757b
weiyx <weason1998@gmail.com>
2022-05-20 22:54:10 +0800

    fix bug in config

--- configs/swin/mask_reppoitsv2_swin_tiny_patch4_window7_mstrain_480_960_giou_gfocal_bifpn_adamw_3x_coco.py
+++ configs/swin/mask_reppoitsv2_swin_tiny_patch4_window7_mstrain_480_960_giou_gfocal_bifpn_adamw_3x_coco.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/mask_reppointsv2_swin_bifpn.py'
+    '../_base_/models/mask_reppointsv2_swin_bifpn.py',
     '../_base_/datasets/coco_instance.py',
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
